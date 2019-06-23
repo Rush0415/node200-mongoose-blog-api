1ccb8c9073dfa6a89b179b4f440304439bee5dc5
rush <rushastronomo0415@gmail.com>
2019-06-23 14:28:21 -0700

    modified app.js

--- server/app.js
+++ server/app.js
@@ -5,15 +5,8 @@ const bodyParser = require('body-parser');
 if(process.env.ENV === 'production') {
     mongoose.connect(process.env.MONGODB_URI);
 }else {
-    mongoose.connect('mongodb://localhost/my-blog');
+    mongoose.connect('mongodb://localhost/my-blog', { useNewUrlParser: true });
 }
-
-// mongoose.connect('mongodb://testdummy:node200@ds341837.mlab.com:41837/heroku_psksqn61',{ useNewUrlParser: true },
-// (err) => {
-//       if(err) {
-//         return console.log(err, 'An internal server error has occured');
-//       }
-//     });
 
     mongoose.Promise = Promise;
 
