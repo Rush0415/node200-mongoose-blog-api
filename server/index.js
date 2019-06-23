a50bcc5e1b041aeb16c97f08cdb990189af1bf92
rush <rushastronomo0415@gmail.com>
2019-06-23 14:19:44 -0700

    modified app.js/index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,4 @@
 const server = require('./app');
-require("dotenv").config();
-const PORT = process.env.PORT
 
-server.listen(8080, function() {
-  console.log(`Server is listening on http://localhost:${PORT}`);
-})
+const PORT = process.env.PORT || 8080
+server.listen(PORT, () => console.log(`Server is listening${PORT}`) )
